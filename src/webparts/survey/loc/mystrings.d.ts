85ecae313fd61e26d5ce0581d1796e301b221d98
NVANH <nvanh@cmcglobal.vn>
2023-03-03 13:23:25 +0700

    first commit

---
+++ src/webparts/survey/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ISurveyWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'SurveyWebPartStrings' {
+  const strings: ISurveyWebPartStrings;
+  export = strings;
+}
